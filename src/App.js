489cb88d5faf28c14f2756c035719c49d49417b3
murtuza <a.murtuza007@gmail.com>
2023-03-22 17:11:48 +1100

    Header component developed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Home } from './Pages/Home'
+import './index.css'
 
 export default function App() {
   return (
-    <div>App</div>
+    <Home />
   )
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import { Header } from '../../Components/header'
+
+export const Home = () => {
+  return (
+    <Container>
+        <Header />
+    </Container>
+  )
+}
